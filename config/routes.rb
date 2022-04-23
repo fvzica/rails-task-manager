cb85f8facab2dfaa0302727d24cc1bb3b65fa7a9
Felipe Zica <felipevzica@gmail.com>
2022-04-23 07:55:40 -0300

    create task

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
-  ## Restaurant Index. Display all tasks
+  ## Task Index. Display all tasks
   get "tasks", to: "tasks#index"
-  ## Restaurant Show. Display a single restaurant
+  ## Task New. Display a form to create a new Task
+  get "tasks/new", to: "tasks#new"
+  ## Task Show. Display a single Task
   get "tasks/:id", to: "tasks#show", as: "task"
+  ## Task Create. Create a new Task
+  post "tasks", to: "tasks#create"
+
 end
