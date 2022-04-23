04faa8e3fe7174d5c8228ee85132e3dc07372af8
Felipe Zica <felipevzica@gmail.com>
2022-04-23 12:07:40 -0300

    all done

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
-  ## Task Index. Display all tasks
-  get "tasks", to: "tasks#index"
-  ## Task New. Display a form to create a new Task
-  get "tasks/new", to: "tasks#new"
-  ## Task Show. Display a single Task
-  get "tasks/:id", to: "tasks#show", as: "task"
-  ## Task Create. Create a new Task
-  post "tasks", to: "tasks#create"
+resources :tasks
 
 end
