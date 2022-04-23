2a983328660e07f6aafe66602112159b7b7d9f6f
Felipe Zica <felipevzica@gmail.com>
2022-04-23 07:39:04 -0300

    index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  ## Restaurant Index. Display all tasks
+  get "tasks", to: "tasks#index"
+  ## Restaurant Show. Display a single restaurant
+  get "tasks/:id", to: "tasks#show", as: "task"
 end
